0efb7a01ce28d7dccfcbfcd49efb24517087ac27
hoaidt118 <danghoaihnue@gmail.com>
2024-05-20 17:53:45 +0700

    Topic 11 - Selenium locator

---
+++ selenium/webdriver/Topic_04_Relative_Locator.java
@@ -0,0 +1,50 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Topic_04_Relative_Locator {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+        }
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com/register");
+
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void  TC_01_Relative(){
+        driver.get("https://demo.nopcommerce.com/login?returnUrl=%2Fregister");
+        //Login button
+        By loginButtonBy = By.cssSelector("button.login-button");
+        WebElement loginButtonElement = driver.findElement(By.cssSelector("button.login-button"));
+        RelativeLocator.with(By.tagName("label"));
+    }
+
+
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
